3c20ab77bb856c736567a7710afcbe2cfd6e8e56
Simon Green <simon.green@thielsen.co.uk>
2019-03-11 12:41:37 +0000

    Added date to transaction class

--- spec/unit/transaction_spec.rb
+++ spec/unit/transaction_spec.rb
@@ -5,10 +5,12 @@ RSpec.describe 'transaction' do
     transaction = Transaction.new('deposit', 100.01)
     expect(transaction.type).to eq 'deposit'
     expect(transaction.amount).to eq 100.01
+    expect(transaction.date).to eq Date.today
   end
   it 'can create new withdrawal transaction' do
     transaction = Transaction.new('withdraw', 99.09)
     expect(transaction.type).to eq 'withdraw'
     expect(transaction.amount).to eq 99.09
+    expect(transaction.date).to eq Date.today
   end
 end
--- transaction.rb
+++ transaction.rb
@@ -1,8 +1,11 @@
-class Transaction
-  attr_reader :type, :amount
+require 'date'
 
-  def initialize(type, amount)
+class Transaction
+  attr_reader :type, :amount, :date
+
+  def initialize(type, amount, date=Date.today)
     @type = type
     @amount = amount
+    @date = date
   end
 end
