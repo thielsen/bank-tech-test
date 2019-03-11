704b0235ce9b9394a97b7f5f48ca11f78eb80b7d
Simon Green <simon.green@thielsen.co.uk>
2019-03-11 16:33:59 +0000

    DRYed tests

--- spec/feature/deposit_withdraw_money_spec.rb
+++ spec/feature/deposit_withdraw_money_spec.rb
@@ -1,15 +1,17 @@
 require './account'
 
 RSpec.describe 'deposit money' do
+  before(:each) do
+    @account = Account.new
+  end
+
   it 'allows us to apply a deposit to our account' do
-    account = Account.new
-    account.credit(100.01)
-    expect(account.transactions).to be_an Array
+    @account.credit(100.01)
+    expect(@account.transactions).to be_an Array
   end
 
   it 'allows us to withdraw from our account' do
-    account = Account.new
-    account.debit(99.09)
-    expect(account.transactions).to be_an Array
+    @account.debit(99.09)
+    expect(@account.transactions).to be_an Array
   end
 end
--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -1,20 +1,21 @@
 require './account'
 
-RSpec.describe 'Account' do
+RSpec.describe Account do
+  before(:each) do
+    @account = described_class.new
+  end
+
   it 'starts with no transactions' do
-    account = Account.new
-    expect(account.transactions).to eq []
+    expect(@account.transactions).to eq []
   end
 
   it 'allows a credit' do
-    account = Account.new
-    account.credit(100.01)
-    expect(account.transactions[0].credit).to eq 100.01
+    @account.credit(100.01)
+    expect(@account.transactions[0].credit).to eq 100.01
   end
 
   it 'allows a debit' do
-    account = Account.new
-    account.debit(100.01)
-    expect(account.transactions[0].debit).to eq 100.01
+    @account.debit(100.01)
+    expect(@account.transactions[0].debit).to eq 100.01
   end
 end
